9c2d22d09f80d38a988e06a557eccbe1bf8e7c1f
Ogbu Esther <ogbuuzoma413@gmail.com>
2023-03-02 18:01:48 +0100

    saved

--- src/savings/Savings.tsx
+++ src/savings/Savings.tsx
@@ -1,80 +1,91 @@
 import React from "react";
 import styled from "styled-components";
+import img from "../Assets/person.png";
+// import Cards from "./Cards";
+// import Todo from "./Todo";
 
 const Savings = () => {
   return (
-    <div>
-      <Container>
-        <Wrapper>
-          <Top>
-            <h1>Savings </h1>
-            <p>Let's see how well you're doing.</p>
-          </Top>
-          <Middle>
-            <One>
-              <p>Total Balance</p>
-              <h1>₦0.00</h1>
-            </One>
-            <Two></Two>
-          </Middle>
-          <Third></Third>
-        </Wrapper>
-      </Container>
-    </div>
+    <Container>
+      <Wrapper>
+        <Top>
+          <Left>
+            <Bold>
+              <h1>Savings</h1>
+            </Bold>
+            <P>
+              <p>Let's see how well you're doing.</p>
+            </P>
+          </Left>
+        </Top>
+        <Middle>
+          <One></One>
+          <Two></Two>
+        </Middle>
+        {/* <Cards />
+        <Todo /> */}
+      </Wrapper>
+    </Container>
   );
 };
 
 export default Savings;
 
-const One = styled.div`
-  width: 70%;
-  height: 70px;
-  border: 1px solid gray;
-
-  p {
-    color: #718096;
-    /* line-height: 24px; */
-    font-size: 18px;
-    font-weight: 400;
-  }
-`;
-
 const Two = styled.div``;
 
-const Third = styled.div``;
-
-const Middle = styled.div``;
-
-const Top = styled.div`
-  margin-right: 190px;
-  h1 {
-    margin: 0;
-    font-family: U8, sans-serif;
-    font-weight: 700;
-    font-size: 30px;
-    /* line-height: 45px; */
-    color: #000000;
-  }
-
-  p {
-    color: #718096;
-    /* line-height: 24px; */
-    font-size: 18px;
-    font-weight: 400;
-  }
+const One = styled.div`
+  /* padding: 100px; */
+  width: 500px;
+  height: 100px;
+  border: 1px solid darkgray;
 `;
 
+const Middle = styled.div`
+  margin-top: 30px;
+`;
+
+const Img = styled.img`
+  height: 45px;
+`;
+const Right = styled.div`
+  display: flex;
+  align-items: center;
+`;
+const P = styled.div`
+  p {
+    color: #718096;
+    font-family: Karla, sans-serif;
+    font-size: 0.875rem;
+    margin: 0;
+  }
+  margin-top: 7px;
+`;
+const Bold = styled.div`
+  h1 {
+    font-size: 29px;
+    color: #000;
+    font-weight: 700;
+    font-family: U8, sans-serif;
+    margin: 0;
+  }
+`;
+const Left = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
 const Wrapper = styled.div`
   width: 92%;
-  height: 100%;
   display: flex;
   flex-direction: column;
   /* justify-content: center; */
-  align-items: center;
+  /* align-items: center; */
   padding-top: 90px;
-  /* background-color: aliceblue; */
 `;
-
+const Top = styled.div`
+  width: 100%;
+  display: flex;
+  /* justify-content: space-between; */
+`;
 const Container = styled.div`
   width: calc(100% - 19%);
   height: 100vh;
