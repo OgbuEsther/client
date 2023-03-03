69a171fee3e16e4f3b21547198097efcd3349dd8
Ogbu Esther <ogbuuzoma413@gmail.com>
2023-03-03 21:40:59 +0100

    saved

--- src/components/Api/Api.tsx
+++ src/components/Api/Api.tsx
@@ -1,1 +1,25 @@
 import axios from "axios";
+
+import { userData } from "../../interfaces/userInterfaces";
+
+const localUrl = "http://localhost:6400";
+
+export const createUser = async ({
+  name,
+  email,
+  password,
+  userName,
+  phoneNumber,
+}: any) => {
+  return await axios
+    .post(`${localUrl}/api/user/signup`, {
+      name,
+      email,
+      password,
+      userName,
+      phoneNumber,
+    })
+    .then((res) => {
+      return res.data;
+    });
+};
