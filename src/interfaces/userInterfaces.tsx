888fe6002e0c3eb072fa3fa285380cbe6477ff09
Ogbu Esther <ogbuuzoma413@gmail.com>
2023-03-03 21:56:07 +0100

    saved

--- src/interfaces/userInterfaces.tsx
+++ src/interfaces/userInterfaces.tsx
@@ -2,11 +2,6 @@ export interface userData {
   name: string;
   email: string;
   password: string;
-  accountNumber: number;
-  phoneNumber: number;
   userName: string;
-  verified: boolean;
-  isAdmin: boolean;
-  wallet: {}[];
-  history: {}[];
+  phoneNumber: number;
 }
